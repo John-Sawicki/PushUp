7ba007ba8507eb62940a2a920855861da0560542
John Sawicki <jpsawicki@gmail.com>
2017-12-09 19:30:53 -0600

    Initial commit

---
+++ app/src/main/java/app/cominjohnsawickitrkhp_identity_photo/linkedin/httpswww/pushup/MainActivity.java
@@ -0,0 +1,16 @@
+package app.cominjohnsawickitrkhp_identity_photo.linkedin.httpswww.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void TestGitHub(boolean saved){
+        boolean save = false;
+    }
+}
